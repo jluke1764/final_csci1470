655aaaa759a5056e7b4622057d33c87e076c5e9e
Jackie Luke <jplski11@gmail.com>
2021-11-30 20:16:56 -0500

    preprocessing started

---
+++ preprocessing.py
@@ -0,0 +1,15 @@
+import pickle
+import numpy as np
+import tensorflow as tf
+import os
+import PIL
+from PIL import image
+
+
+"""
+resize images to specifized shape, turn image files into bytes
+"""
+def make_datasets(filepath, width, height):
+
+
+
